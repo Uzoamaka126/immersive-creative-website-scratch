2f6a960d7e7820b108e027d8b6ce799b36989ceb
Uzoamaka126 <uzoamakaanyanwu26@gmail.com>
2023-10-13 12:04:16 +0100

    create more pages and setup views on express

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug')
 
 app.get("/", function (req, res) {
-    res.send("<strong>Hello world!</strong>")
+    res.render('template');
+})
+
+app.get("/about", function (req, res) {
+    res.render('about');
+})
+
+app.get("/collections", function (req, res) {
+    res.render('collections');
+})
+
+app.get("/detail/:", function (req, res) {
+    res.render('about');
 })
 
 app.listen(port, function() {
