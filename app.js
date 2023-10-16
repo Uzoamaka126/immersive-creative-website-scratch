8fc0bfb50a4e4d31217441dee596425522d5e349
Uzoamaka126 <uzoamakaanyanwu26@gmail.com>
2023-10-16 12:32:45 +0100

    feat: add more styles

--- app.js
+++ app.js
@@ -46,6 +46,8 @@ app.use(function (req, res, next) {
     next()
 })
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
